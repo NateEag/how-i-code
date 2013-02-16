03e35c2af1f6c3553fcb749a6009928aaec72994
Nate Eagleson <nate@nateeag.com>
2013-02-16 16:38:46 -0500

    Add notmyidea theme under new name (intended for customization)
    This site is not meant as a blog, so I plan to remove a lot of info
    from the theme that just isn't relevant for my purposes.

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,14 +10,6 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
 
-# Blogroll
-# LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-#           ('Python.org', 'http://python.org'),
-#           ('Jinja2', 'http://jinja.pocoo.org'),
-#           ('You can modify those links in your config file', '#'),)
-
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
+
+THEME = 'themes/howicode'
