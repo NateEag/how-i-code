b3d6b8ba78f013013c27a056354a0b18c4579d0d
Nate Eagleson <nate@nateeag.com>
2014-07-15 22:21:39 -0400

    make-virtualenv.sh is more robust

--- make-virtualenv.sh
+++ make-virtualenv.sh
@@ -2,8 +2,9 @@
 
 # A simple script to create this project's virtualenv.
 
-virtualenv pythonenv
-source pythonenv/bin/activate
+project_root=$(dirname "$0")
+virtualenv "$project_root/pythonenv"
+. "$project_root/pythonenv/bin/activate"
 
 easy_install pip
 
