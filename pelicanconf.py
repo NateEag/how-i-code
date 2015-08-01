061be82272a033007dd97d42ac7ed7010495710e
Nate Eagleson <nate@nateeag.com>
2015-07-31 23:44:32 -0400

    Disable Piwik, since my instance has been down for years

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,5 +20,8 @@ JINJA_EXTENSIONS = ['jinja2.ext.ExprStmtExtension']
 # Note that this is protocol-relative, since the current Piwik snippet decides
 # which to use dynamically.
 # ...not that this site actually has any secure URLs...
-PIWIK_URL = '//www.nateeag.com/analytics/'
-PIWIK_SITE_ID = 1
+# TODO Uncomment these once I bring a Piwik instance online again.
+#PIWIK_URL = '//www.nateeag.com/analytics/'
+#PIWIK_SITE_ID = 1
+PIWIK_URL = False
+PIWIK_SITE_ID = False
