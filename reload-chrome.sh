6d2940e148b9425a925e4b4292b3bcf10197a664
Nate Eagleson <nate@nateeag.com>
2018-01-23 22:59:44 -0500

    Note that chromix-too would allow porting reload-chrome.sh to Linux
    At least it looks like it should, and fortunately for us programmers
    theory and practice never vary at all.

--- reload-chrome.sh
+++ reload-chrome.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
-# Tell Chrome to reload the URL passed in the first argument, via AppleScript.
+# Tell Chrome to reload the URL passed in the first argument.
+#
+# Currently depends on AppleScript. Should I ever need to support it on Linux
+# (for instance, if I finally run screaming from the land of Apple),
+# chromix-too should do the job: https://github.com/smblott-github/chromix-too
 
 script_dir=$(dirname "$0")
 
